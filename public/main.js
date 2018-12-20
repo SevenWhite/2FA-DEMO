2d30dac7ab22d5c69c1e48d36278f1942120b948
sevenwhite <album.777.leo@gmail.com>
2018-12-20 13:44:22 +0300

    add public folder and vue

--- public/main.js
+++ public/main.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    users: []
+  },
+  mounted() {
+    fetch('/users')
+        .then(resp => console.log('users', resp.data))
+        .catch(err => console.error(err))
   }
-})
+});
